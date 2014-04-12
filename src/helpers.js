15a49684a245cb7b13d0fa573582290b4ae021ab
Matthew Robb <matthewwrobb@gmail.com>
2014-04-12 11:28:17 -0700

    See if we can merge these guys

--- src/helpers.js
+++
@@ -1,8 +0,0 @@
-var extend = exports.extend = function (object, properties) {
-    var key, val;
-    for (key in properties) {
-        val = properties[key];
-        object[key] = val;
-    }
-    return object;
-};
