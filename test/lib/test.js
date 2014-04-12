df166194d0a082c4c3f21f4f5eb5bc18f13aa9e0
Matthew Robb <matthewwrobb@gmail.com>
2014-04-12 15:28:20 -0700

    Getting macros hooked in

--- test/lib/test.js
+++ test/lib/test.js
@@ -1,5 +1,21 @@
-var x = {
-        foo: 'bar',
-        what: 'up'
+if (typeof exports === 'object' && typeof define !== 'function') {
+  var define = function (factory) {
+    factory(require, exports);
+  };
+}
+define(function (require, exports) {
+  "use strict";
+  var x = {
+      foo: "bar",
+      what: "upBRO"
     };
-var object = x, foo = object.foo;
+  var object = x, foo = object.foo;
+  var list = [
+      1,
+      2,
+      3
+    ];
+  console.log(list.map(function (i) {
+    return i * i;
+  }.bind(this)));
+});
